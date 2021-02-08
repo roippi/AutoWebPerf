c277b457f675350a581138cf424131c3b14301e4
Ben Roberts <beroberts@wayfair.com>
2021-02-08 13:17:12 -0500

    back off to 500ms timeout

--- src/extensions/throttle-extension.js
+++ src/extensions/throttle-extension.js
@@ -22,7 +22,7 @@ class ThrottleExtension extends Extension {
 
   afterRun(context, options) {
     return new Promise ((r) => {
-      setTimeout(() => r(), 420);
+      setTimeout(() => r(), 500);
     });
   }
 }
